85c4ce9c1e536840e53e7c77ba5db346c676d79e
scepion4 <safonov04@ukr.net>
2023-01-23 18:11:38 +0200

    feat:resolve task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const MORSE_TABLE = {
+    '*':' ',
     '.-':     'a',
     '-...':   'b',
     '-.-.':   'c',
@@ -38,7 +39,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let j=0;
+    let k=10
+    let arr=[]
+    let round=expr.length/10
+    for(let i=0;i<round;i++){
+      let subStr=expr.slice(j,k)
+      k=k+10
+      j=j+10
+      arr.push(subStr)
+    }
+    let newArr=arr.map(e=>e.slice(e.indexOf('1'),e.length))
+    let dot=newArr.map(e=>e.replaceAll('10','.'))
+    let dashed=dot.map(e=>e.replaceAll('11','-'))
+    let text=dashed.map(e=>e=MORSE_TABLE[e])
+    let result=text.join('')
+    return result
 }
 
 module.exports = {
